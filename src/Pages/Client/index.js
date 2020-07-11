fa9c090837fc3c48b556c88dcf51513827cb6dfb
Augusto <aht@cin.ufpe.br>
2020-07-11 16:41:26 -0300

    Getting user info

--- src/Pages/Client/index.js
+++ src/Pages/Client/index.js
@@ -4,14 +4,49 @@ import Navbar from '../../Components/Navbar/index';
 
 import Footer from '../../Components/Footer/index';
 
+
+import api from '../../Services/api';
+import { getToken } from '../../Services/auth';
+
 class client extends Component{
+    
+    constructor(){
+        super();
+
+        this.state = {
+            user: {}
+        }
+
+    }
+
+    async componentDidMount(){
+        
+        const my_token = getToken();
+
+        var response = await api.get('/user', {
+            headers: { authorization: 'Bearer ' + my_token }
+        })
+
+        const _id = response.data.user_id;
+        
+        response = await api.post('/user', {_id},{
+            headers: { authorization: 'Bearer ' + my_token }
+        })
+
+        const user = response.data.user;
+        
+        this.setState({ user });
+
+    }
 
     render(){
         
+        const { user } = this.state;
+
         return(
             <div className="container-client">
                 <Navbar />
-                <h1>Logado!</h1>
+                <h1>Oi, {user.Name}!</h1>
                 <Footer />
             </div>
         );
